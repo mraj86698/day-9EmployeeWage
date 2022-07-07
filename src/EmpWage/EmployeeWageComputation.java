7aea2ae6eec099e4ac26e09946bc9205d3549e99
MohanRaj <mraj86698@gmail.com>
2022-07-07 18:15:46 +0530

    Added to Compute Employee Wage for Multiple Companies

---
+++ src/EmpWage/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package EmpWage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("UC-8 Compute Employee Wage for Multiple Companies");
+
+	}
+
+}
